ef573b6487875abef1ca314993e3e032374565d2
Rana <rbansal@college.harvard.edu>
2018-12-02 22:52:38 -0500

    make edits to add new iron and steel section

--- shiny/dashboard.R
+++ shiny/dashboard.R
@@ -8,11 +8,30 @@ library(dplyr)
 library(DT)
 library(leaflet)
 library(rgdal)
+library(scales)
 
 # Loading Chart Data
 partners <- read_rds("top_partners_simp.rds")
 qpal <- colorQuantile("Blues", NULL, n = 7)
 commodities <- read_rds("commodity_totals.rds")
+alpha_two <- read.csv("Alphas2.csv")
+alpha_two <- alpha_two %>%
+  mutate(newcol = case_when(Import.Country == "United States of America" ~ 1,
+                            Import.Country == "United Arab Emirates" ~ 2,
+                            Import.Country == "China, Hong Kong SAR" ~ 3,
+                            Import.Country == "China" ~ 4,
+                            Import.Country == "Singapore" ~ 5,
+                            Import.Country == "United Kingdom" ~ 6,
+                            Import.Country == "Germany" ~ 7,
+                            Import.Country == "Netherlands" ~ 8,
+                            Import.Country == "Belgium" ~ 9,
+                            Import.Country == "Japan" ~ 10,
+                            Import.Country == "South Africa" ~ 11,
+                            Import.Country == "Turkey" ~ 12,
+                            Import.Country == "Italy" ~ 13,
+                            Import.Country == "France" ~ 14,
+                            Import.Country == "Malaysia" ~ 15,
+                            Import.Country == "Spain" ~ 16))
 
 # Define UI for the application
 ui <- dashboardPage(
@@ -21,7 +40,9 @@ ui <- dashboardPage(
     sidebarMenu(
       menuItem("Home", tabName = "dashboard", icon = icon("home")),
       menuItem("Top Partners by Year", tabName = "graphs", icon = icon("align-left")),
-      menuItem("Partner Ranking (Table)", tabName = "exports", icon = icon("ship"))
+      menuItem("Partner Ranking (Table)", tabName = "exports", icon = icon("ship")),
+      menuItem("Top Commodities by Year", tabName = "commodities", icon = icon("balance-scale")),
+      menuItem("Iron and Steel Snapshot", tabName = "snapshot", icon = icon("camera"))
     )
   ),
   dashboardBody(
@@ -56,11 +77,33 @@ ui <- dashboardPage(
       tabItem(tabName = "exports",
               h2("India Top Export Partners"),
               box(
-                selectInput("year_choice", "Year:", 
+                selectInput("year_choice2", "Year:", 
                             choices = c(2014, 2015, 2016, 2017)),
                 helpText("Data from United Nations COMTRADE Database.")
               ),
               DT::dataTableOutput("distPlot")
+      ),
+      
+      tabItem(tabName = "commodities",
+              h2("India Top Commodities"),
+              box(
+                selectInput("year_choice3", "Year:",
+                            choices = c(2014, 2015, 2016, 2017)),
+                helpText("Commodities sorted by two-digit HS international code.")
+              ),
+              DT::dataTableOutput("commodityPlot")
+      ),
+      
+      tabItem(tabName = "snapshot",
+              h2("Iron and Steel Exports"),
+              box(
+                sliderInput("countries",
+                            "Number of Countries:",
+                            min = 1,
+                            max = 15,
+                            value = 10)
+              ),
+              plotOutput("complot1", height = 400)
       )
     )
   )
@@ -70,7 +113,7 @@ server <- function(input, output) {
   
   datareact <- reactive({
     partners %>% 
-      filter(year == input$year_choice) %>%
+      filter(year == input$year_choice2) %>%
       arrange(desc(yearly_total))
   })
   
@@ -82,11 +125,27 @@ server <- function(input, output) {
       head(10)
   })
   
+  datareact3 <- reactive({
+    commodities %>% 
+      filter(year == input$year_choice3) %>%
+      arrange(desc(commodity_total)) %>%
+      head(15)
+  })
+  
+  datareact4 <- reactive({
+    alpha_two %>%
+      filter(newcol <= input$countries)
+  })
+  
   output$distPlot <- DT::renderDataTable({
     datareact() %>%
       filter(partner != "World") %>%
-      datatable(colnames = c('Year', 'Trade Partner', 'Trade Value (Billions USD)')) 
-
+      datatable(colnames = c('Year', 'Trade Partner', 'Trade Value (Billions USD)'))
+  })
+  
+  output$commodityPlot <- DT::renderDataTable({
+    datareact3() %>%
+      datatable(colnames = c('Year', 'Commodity', 'Name', 'Total Trade Value (USD)')) 
   })
   
   output$distPlot2 <- renderPlot({
@@ -98,6 +157,18 @@ server <- function(input, output) {
       geom_text(aes(label = yearly_total), vjust = 1.5, colour = "white", size = 3) +
       labs(x = NULL, y = "Trade Value (Billions USD)") + 
       theme(axis.text.x = element_text(angle = 60, hjust = 1))
+  })
+  
+  output$complot1 <- renderPlot({
+    datareact4() %>%
+      filter(!Alpha > 1) %>%
+      mutate(Month = as.Date(paste("01", Month,sep = "-"),"%d-%b-%y")) %>%
+      filter(Commodity == "Iron and steel") %>%
+      ggplot(aes(x = Month, y = Alpha, color = Import.Country)) + geom_point() +
+      xlab("Date") +
+      ylab("Fraction of Destination Country Imports from India") +
+      ggtitle("Iron and Steel") +
+      labs(caption = "Export Data from United Nations COMTRADE Database")
   })
   
   output$mymap <- renderLeaflet({
